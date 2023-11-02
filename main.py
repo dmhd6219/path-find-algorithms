5b03d39f7e2822af8de9ce91d79b9ee80119d46e
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-11-02 20:57:34 +0300

    add beautiful print to field and node

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from utils.types import NodeType
 class Node:
     __x: int
     __y: int
-    __type: list[NodeType] = []
+    __type: list[NodeType]
 
     def __init__(self, x: int, y: int, type_: NodeType = None):
         self.__x = x
         self.__y = y
+        self.__type = []
 
         if type_ is not None:
             self.__type.append(type_)
@@ -25,13 +26,19 @@ class Node:
     def y(self) -> int:
         return self.__y
 
+    def __repr__(self):
+        return f'Node{{X={self.__x};Y={self.__y};Info:[{",".join(map(lambda x: str(x).split(".")[-1], self.__type))}];}}'
+
 
 class Thanos:
     __perception_type: int
-    __x: int = 0
-    __y: int = 0
+    __x: int
+    __y: int
 
     def __init__(self, perception_type: int):
+        self.__x = 0
+        self.__y = 0
+
         self.__perception_type = perception_type
 
     def get_perception_type(self) -> int:
@@ -58,16 +65,17 @@ class Thanos:
 class Map:
     __map: list[list[Node]]
     __thanos: Thanos
-    __steps: int = 0
+    __steps: int
 
-    def __init__(self, perception_type: int, stone: Node):
+    def __init__(self, perception_type: int, stone: tuple[int, int]):
         self.thanos = Thanos(perception_type)
         self.__map = [[Node(x, y) for y in range(0, 9)] for x in range(0, 9)]
-        self.__map[stone.x][stone.y] = stone
+        self.__map[stone[0]][stone[1]].add_info(NodeType.STONE)
+        self.__steps = 0
 
     def get_node(self, x: int, y: int):
         if (not 0 <= x <= 9) or (not 0 <= y <= 9):
-            raise IndexError("Map is 8x8")
+            raise IndexError(f"Map is 8x8, Point ({x}, {y}) is out of bounds")
 
         return self.__map[x][y]
 
@@ -80,13 +88,17 @@ class Map:
         self.__steps += 1
         return self.__map[move_coords[0]][move_coords[1]]
 
+    def __repr__(self):
+        max_width = max([max(len(str(element)) for element in row) for row in self.__map])
+
+        return "\n".join(" | ".join(str(element).ljust(max_width) for element in row) for row in self.__map)
+
 
 def main() -> None:
     perception_type = int(input())
-    stone_coords = [int(x) for x in input().split()]
+    x, y = [int(x) for x in input().split()]
 
-    field = Map(perception_type, Node(*stone_coords, type_=NodeType.STONE))
+    field = Map(perception_type, (x, y))
 
 
-if __name__ == "__main_":
-    main()
+main()
